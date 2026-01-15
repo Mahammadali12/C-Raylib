a403aca6f85021d1f6e85f366df2200ef7a558cc
Mahammadali12 <zamanli.mehemmedeli@gmail.com>
2026-01-15 16:07:51 -0500

    working metric systems

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 #include <raymath.h>
 #include <stdlib.h>
 
-#define WIDTH 800
-#define HEIGHT 600
-
+#define WIDTH 1500
+#define HEIGHT 1000
+#define PPM  50  //? Pixels_Per_Meter
 #define FRICTION_COEFFICIENT 0.7
 #define DRAG_COEFFICIENT 0.38
 // gcc main.c -g -o bin/main -Wall -Wextra -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
@@ -14,12 +14,12 @@ typedef struct Circle
 {
     Vector2 pos;
     Vector2 vel;
-    Vector2 acc;
-    float scale;
-    float mass;
+    Vector2 acc; 
+    float scale; //*  radius for meter
+    float mass; //* kg
 }Circle;
 
-Circle circle1 = {.pos = {WIDTH/2,HEIGHT/2},.vel = {0,0},.acc = {0,0}, 30.0, 10};
+Circle circle1 = {.pos = {WIDTH/2,HEIGHT/2},.vel = {0,0},.acc = {0,0}, 1.0, 1};
 // Circle circle2 = {.pos = {WIDTH/2+100,HEIGHT/2},.vel = {0,0},.acc = {0,0}, 90.0,10};
 Vector2 gravity = {0,9.81};
 
@@ -74,7 +74,7 @@ int main ()
 
 void draw()
 {
-    DrawCircle(circle1.pos.x,circle1.pos.y,circle1.scale,GREEN);
+    DrawCircle(circle1.pos.x,circle1.pos.y,circle1.scale * PPM,GREEN);
     // DrawCircle(circle2.pos.x,circle2.pos.y,circle2.scale,WHITE);
 }
 
@@ -91,12 +91,19 @@ void updateCircle(Circle* c)
     applyDragForce(c);
 
     float dt = GetFrameTime();
-
+    printf("DT-%f\n",dt);
     c->vel = Vector2Add(c->vel, Vector2Scale(c->acc,dt));
-    c->pos = Vector2Add(c->pos, Vector2Scale(c->vel,dt));
-    printf("y- %f\n",c->vel.y);
+    c->pos = Vector2Add(c->pos, Vector2Scale(c->vel,PPM));
+    // c->pos = Vector2Add(c->pos, Vector2Scale(c->vel,dt));
+    printf("ACC: x-%f || y-%f\n",c->acc.x,c->acc.y);
+    printf("VEL: x-%f || y-%f\n",c->vel.x,c->vel.y);
+    printf("POS: x-%f || y-%f\n",c->pos.x,c->pos.y);
+    c->acc = Vector2Zero();
 
-    c->acc = (Vector2){0,0};
+    // c->vel = Vector2Add(c->vel, c->acc);
+    // c->pos = Vector2Add(c->pos, c->acc);
+    // printf("y- %f\n",c->vel.y);
+
 
 }
 
@@ -108,8 +115,10 @@ void applyForce(Circle* c, Vector2 force)
 
 void applyGravity(Circle* c, Vector2 gravity)
 {
-    gravity = Vector2Scale(gravity,c->mass);
-    applyForce(c,gravity);
+    // gravity = Vector2Scale(gravity,c->mass);
+    // applyForce(c,gravity);
+
+    c->acc = Vector2Add(c->acc,gravity); //* Alternate Method
 
 }
 
@@ -124,7 +133,6 @@ void applyFriction(Circle* c)
     applyForce(c,friction);
 
 }
-
 
 void checkBounds(Circle* c)
 {
@@ -164,7 +172,6 @@ void checkBounds(Circle* c)
     }
 }
 
-
 void contactEdge(Circle* c)
 {
     if (c->pos.y > HEIGHT - c->scale - 1 )
@@ -178,16 +185,16 @@ void contactEdge(Circle* c)
 void applyDragForce(Circle* c)
 {
     // -1/2 * surfaceArea * airDensity * dragCoefficient
-    // float surfaceArea = PI * c->scale * c->scale; //surface area of halh circle;
-    // float airDensity = 1.225;
+    float surfaceArea = PI * c->scale * c->scale; //surface area of halh circle;
+    float airDensity = 1.225;
     float speed = Vector2Length(c->vel);
 
-    float dragMagnitude = 1 * speed * speed;
+    // float dragMagnitude = 1 * speed * speed;
+    float dragMagnitude = 0.5 * surfaceArea * airDensity * DRAG_COEFFICIENT * speed * speed;
 
     Vector2 dragForce = Vector2Normalize(c->vel);
     dragForce = Vector2Negate(dragForce);
 
     dragForce = Vector2Scale(dragForce,dragMagnitude);
-    printf("%f || %f\n",dragForce.x,dragForce.y);
     applyForce(c,dragForce);
 }
